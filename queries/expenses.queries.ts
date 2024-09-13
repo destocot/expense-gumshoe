36bb58c83934b76c4246553f1b77668dd2cc702a
Khurram Ali <khurramcali@gmail.com>
2024-09-13 14:12:03 -0400

    time crunch setup

--- queries/expenses.queries.ts
+++ queries/expenses.queries.ts
@@ -3,15 +3,43 @@ import { validateRequest } from '@/lib/validate-request'
 import ExpenseModel, { Expense } from '@/models/Expense'
 import { redirect } from 'next/navigation'
 
-export const findExpenses = async (): Promise<Array<Expense>> => {
+export const findExpenses = async (options?: {
+  time?: 'day' | 'week' | 'month'
+}): Promise<Array<Expense>> => {
   const { user: authUser } = await validateRequest()
   if (!authUser) redirect('/login')
 
+  const filter = { userId: authUser.id }
+
+  if (options?.time) {
+    const time = options.time
+
+    const now = new Date()
+    let startDate
+
+    switch (time.toLowerCase()) {
+      case 'day':
+        startDate = new Date(now.getFullYear(), now.getMonth(), now.getDate())
+        break
+      case 'week':
+        startDate = new Date(
+          now.getFullYear(),
+          now.getMonth(),
+          now.getDate() - (now.getDay() || 7),
+        )
+        break
+      case 'month':
+        startDate = new Date(now.getFullYear(), now.getMonth(), 1)
+        break
+    }
+
+    // @ts-ignore
+    filter.createdAt = { $gte: startDate, $lt: now }
+  }
+
   await dbConnect()
 
-  const expenseDocs = await ExpenseModel.find({
-    userId: authUser.id,
-  }).sort({
+  const expenseDocs = await ExpenseModel.find(filter).sort({
     createdAt: -1,
   })
 
